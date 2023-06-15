414020054de795c0348223526d71b8cbad8e3ad4
Hadley99 <hadleyfernandes99@gmail.com>
2023-06-15 12:34:48 +0530

    removed unused vars

--- awesome/config/theme.lua
+++ awesome/config/theme.lua
@@ -43,7 +43,6 @@ theme.border_marked = "#91231c"
 
 -- Generate taglist squares:
 local taglist_square_size = dpi(6)
-local taglist_square_radius = dpi(2)
 
 theme.taglist_squares_sel = theme_assets.taglist_squares_sel(taglist_square_size, theme.fg_normal)
 theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
